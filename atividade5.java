a67737a56eea759e95c0157821c9f39059602aa2
Vinicius Magnus <viniciusmagnus11@gmail.com>
2022-06-27 11:24:24 -0300

    Add files via upload

---
+++ atividade5.java
@@ -0,0 +1,9 @@
+public class numeros_primos {
+    public static void main(String[] args) {
+        for (int i = 2; i < 123; i++) { // 1 não é primo
+            if ((i == 2) || (i == 3) || (i % 2 != 0) && (i % 3 != 0)) {
+                System.out.println(i + " é um número primo.");
+            }
+        }
+    }
+}
