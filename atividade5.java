eedb9b83a37437a797a2d3b4603da801901342b2
Vinicius Magnus <viniciusmagnus11@gmail.com>
2022-07-04 12:29:22 -0300

    Update atividade5.java

--- atividade5.java
+++ atividade5.java
@@ -1,4 +1,4 @@
-public class numeros_primos {
+public class atividade5 {
     public static void main(String[] args) {
         for (int i = 2; i < 123; i++) { // 1 não é primo
             if ((i == 2) || (i == 3) || (i % 2 != 0) && (i % 3 != 0)) {
