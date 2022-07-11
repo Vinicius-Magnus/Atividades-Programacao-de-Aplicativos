f7e4c30ffc7a5c1216a7e88bdeec778bf7229170
Vinicius Magnus <viniciusmagnus11@gmail.com>
2022-07-11 11:15:55 -0300

    Update atividade9.java

--- atividade9.java
+++ atividade9.java
@@ -8,19 +8,19 @@ public class atividade9{
      idade = entrada.nextInt();  
      entrada.close();
      if (idade <= 3){
-        JOptionPane.showMessageDialog(null,"bebê");
+        JOptionPane.showMessageDialog(null,"Você é bebê");
      } else if ((idade >3) && (idade<=9)){
-        JOptionPane.showMessageDialog(null,"criança");
+        JOptionPane.showMessageDialog(null,"Você é criança");
      }  else if ((idade >9) && (idade<=13)){
-        JOptionPane.showMessageDialog(null,"pré-adolescente");
+        JOptionPane.showMessageDialog(null,"Você é pré-adolescente");
     } else if ((idade >13) && (idade<=17)){
-        JOptionPane.showMessageDialog(null,"adolescente");
+        JOptionPane.showMessageDialog(null,"Você é adolescente");
     } else if ((idade > 17) && (idade<=25)){
-        JOptionPane.showMessageDialog(null,"jovem adulto");
+        JOptionPane.showMessageDialog(null,"Você é jovem adulto");
     }  else if ((idade >25) && (idade<=39)){
-        JOptionPane.showMessageDialog(null,"adulto");
+        JOptionPane.showMessageDialog(null,"Você é adulto");
     }  else if (idade >39){
-        JOptionPane.showMessageDialog(null,"💀");
+        JOptionPane.showMessageDialog(null,"Você é velho 💀");
     }
     }
 }
